17d13bab84787527cfa50d13151baaea4a3b468f
Emil Rofors <emirof@gmail.com>
2019-04-10 00:03:35 +0200

    Fixed LTF motor control

--- gui/nano_scanner.py
+++ gui/nano_scanner.py
@@ -10,22 +10,16 @@ import psutil #to check if EFU is running
 class Scanner():
     """class for scanning MAPMT in the nano chamber in the micro-beam hall"""
     def __init__(self, laser_setup = True, nano_setup = False):
-        self.path_to_libraries = '/home/seian/LaserScan'
-        self.output_directory = '/media/emil/NTFS_partition/IFE_june_2018/data'
+        self.path_to_libraries = '/home/erofors/LaserScan'
+        self.output_directory = '/home/data'
         if laser_setup:
             self.steps_per_mm = 1
         elif nano_setup:
             self.steps_per_mm = 117647
         self.laser_setup = laser_setup
         self.nano_setup = nano_setup
-        self.dir_to_make = f"{output_directory}/{runname}"
         self.motor_zero_x = 0
         self.motor_zero_y = 0
-        if not os.path.exists(dir_to_make):
-            os.mkdir(dir_to_make)
-        else:
-            print(f"Error: Directory {self.dir_to_make} already exists! Exiting")
-            sys.exit()
         if nano_setup:
             print('Initiating and homeing nano motors...')
             nc.connect()
@@ -36,14 +30,16 @@ class Scanner():
             print('Homeing laser motors...')
             # TODO check if Z motor needs to be moved to somewhere...
             time.sleep(5)
-    def set_motor_translation(self, motor_x, motor_t, mapmt_x, mapmt_y):
+    def set_motor_translation(self, motor_x, motor_y, mapmt_x, mapmt_y):
         """
         Enter the motor and corresponding MAPMT positions for some position
         and absolute motor positions for MAPMT (0,0) will be calculated.
         Don't consider the 0.25 mm extra of the edge pixels.
         """
-        self.motor_zero_x = motor_x - mapmt_x*self.steps_per_mm
+        self.motor_zero_x = motor_x - mapmt_x*self.steps_per_mm # for nano motors
+        self.motor_zero_x = motor_x + mapmt_x*self.steps_per_mm #for LTF motors
         self.motor_zero_y = motor_y + mapmt_y*self.steps_per_mm
+        print(f"motor zero x:{self.motor_zero_x}, y:{self.motor_zero_y}")
     def to_MAPMT_xy(self, motor_x, motor_y):
         """takes motor coordinates and returns MAPMT x,y coordinates (string)"""
         x = 0 - (motor_x - motor_zero_x) / self.steps_per_mm
@@ -51,8 +47,12 @@ class Scanner():
         return "{0:.2f}".format(x), "{0:.2f}".format(y)
     def to_motor_xy(self, mapmt_x, mapmt_y):
         """takes MAPMT coordinates and returns motor absolute x,y positions"""
-        motor_x = int(self.motor_zero_x + mapmt_x * self.steps_per_mm)
-        motor_y = int(self.motor_zero_y + mapmt_y * self.steps_per_mm)
+        if self.nano_setup:
+            motor_x = int(self.motor_zero_x + mapmt_x * self.steps_per_mm)
+            motor_y = int(self.motor_zero_y + mapmt_y * self.steps_per_mm)
+        if self.laser_setup:
+            motor_x = int(self.motor_zero_x - mapmt_x * self.steps_per_mm)
+            motor_y = int(self.motor_zero_y - mapmt_y * self.steps_per_mm)
         return motor_x, motor_y
     def measure_pedestals(self):
         """ TODO implement """
@@ -72,14 +72,20 @@ class Scanner():
     def move_laser_motors(self, x, y):
         """Takes MAPMT coordinates and moves laser setup to that position"""
         motor_x, motor_y = self.to_motor_xy(x, y)
-        print(f"Moving to X:{x}")
-        subprocess.call(self.path_to_libraries + f"/work M 1 {motor_x}")
-        time.sleep(5)
-        print(f"Moving to Y:{y}")
-        subprocess.call(self.path_to_libraries + f"/work M 2 {motor_y}")
-        time.sleep(5)
+        print(f"Moving to X:{x} which is {motor_x}")
+        subprocess.call([self.path_to_libraries + f"/work", "M", "1", f"{motor_x}"])
+        time.sleep(1)
+        print(f"Moving to Y:{y} which is {motor_y}")
+        subprocess.call([self.path_to_libraries + f"/work", "M", "2", f"{motor_y}"])
+        time.sleep(1)
     def scan(self, x_positions = range(10,20), y_positions = range(10,20), runname='runname', VME_daq = True, EFU_daq = False, laser_setup = True, nano_setup = False):
         """performs a scan """
+        self.dir_to_make = f"{output_directory}/{runname}"
+        if not os.path.exists(dir_to_make):
+            os.mkdir(dir_to_make)
+        else:
+            print(f"Error: Directory {self.dir_to_make} already exists! Not performing scan!")
+            return 1
         for x in x_positions:
             for y in y_positions:
                 filename = f"{self.output_directory}/{self.runname}/X{x}Y{y}_scan"
